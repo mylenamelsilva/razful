37ea5539567893f3e837577c6626b75642f1f250
mylena mel <mylenamsfarias@gmail.com>
2024-11-07 23:38:29 -0300

    feat: front de alunos no razor criada

---
+++ src/Front/Controllers/TurmasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front.Controllers
+{
+    public class TurmasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
